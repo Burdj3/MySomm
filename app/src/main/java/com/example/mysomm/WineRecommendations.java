d9e5d0e5c4710a14eb77efa255a7d18d19a9475b
Srinath <srinath.subramanian1@gmail.com>
2021-03-25 21:17:44 -0500

    Changed the dimensions on the wine recommendations and made the layout be on long column instead of rows and columns.

--- app/src/main/java/com/example/mysomm/WineRecommendations.java
+++ app/src/main/java/com/example/mysomm/WineRecommendations.java
@@ -2,6 +2,7 @@ package com.example.mysomm;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -22,14 +23,16 @@ public class WineRecommendations extends AppCompatActivity {
         ArrayList<Wine> wines = new ArrayList<>();
         wines.add(new Wine("Oaked Chardonnay", "Type: White", "Taste: Sweet", "Oaked Chardonnays are rich, full-bodied and have additional flavors of vanilla, butter and even caramel from the oak. A cool climate, buttery Chardonnay will have more citrus flavors versus a warm climate Chardonnay, which will have more tropical fruit flavors."));
         wines.add(new Wine("Carbernet Sauvignon","Type: Red","Taste: Dry", "Carbernet Sauvignon is one of the most popular red wine grapes. This wine tends to be robust with medium acidity and tannins and flavors of rich, dark fruit. It has slightly elevated levels of alcohol for red wine and pairs well with food."));
-        wines.add(new Wine("Chenin Blanc", "Type: White", "Dry", "Chenin Blanc is very diverse in its styles, from sparkling wines and lean, dry whites to sweet, golden nectars and brandy. It is often known for its notes of honey and straw as well as a palate that is rich in stone-fruit flavors."));
-        wines.add(new Wine("Shiraz", "Type: Red", "Sweet", "Shiraz wines are well known for their rich and juicy mid-palate. You can expect to taste flavours of pepper, spice and sweet red fruit. We also often get a different feel on the palate, strong on the front in the mouth and on the finish but with a slightly different mid-palate feel."));
+        wines.add(new Wine("Chenin Blanc", "Type: White", "Taste: Dry", "Chenin Blanc is very diverse in its styles, from sparkling wines and lean, dry whites to sweet, golden nectars and brandy. It is often known for its notes of honey and straw as well as a palate that is rich in stone-fruit flavors."));
+        wines.add(new Wine("Shiraz", "Type: Red", "Taste: Sweet", "Shiraz wines are well known for their rich and juicy mid-palate. You can expect to taste flavours of pepper, spice and sweet red fruit. We also often get a different feel on the palate, strong on the front in the mouth and on the finish but with a slightly different mid-palate feel."));
 
         WineRecyclerViewAdapter adapter = new WineRecyclerViewAdapter(this);
         adapter.setWines(wines);
 
         wineRecyclerView.setAdapter(adapter);
-        wineRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        wineRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //wineRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
 
     }
 }
