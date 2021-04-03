4e21228d08bd68101d97e75ceffb7172f995710e
Jack <burdj3@gmail.com>
2021-04-03 16:32:15 -0500

    Added 5 wines to wine databse

--- app/src/main/java/com/example/mysomm/MainActivity.java
+++ app/src/main/java/com/example/mysomm/MainActivity.java
@@ -44,10 +44,11 @@ public class MainActivity extends AppCompatActivity {
     public void fillDatabase()
     {
         dbHelper.deleteAllRows();
-        dbHelper.saveToLocalDatabase("Pacific Rim","Riesling","3","It's pretty good", "1", "White", "Oregon", "3", "10", "https://upload.wikimedia.org/wikipedia/en/c/c0/Red_Wine_Glass.jpg");
-        dbHelper.saveToLocalDatabase("Example 2","idk","1","It's alright", "2", "Red", "Texas", "2", "20", "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX2879719.jpg");
-        dbHelper.saveToLocalDatabase("Example 3","Riesling","3","It's pretty good", "1", "White", "Oregon", "3", "10", "https://upload.wikimedia.org/wikipedia/en/c/c0/Red_Wine_Glass.jpg");
-        dbHelper.saveToLocalDatabase("Example 4","idk","1","It's alright", "2", "Red", "Texas", "2", "20", "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX2879719.jpg");
+        dbHelper.saveToLocalDatabase("Pacific Rim Sweet Riesling","Riesling","4","Moderately sweet with flavors of pineapple, peach, and hints of honey. The finish is bright and crisp which cleanses the palate and provides good balance to the upfront fruit.", "2", "White", "Columbia Valley, Washington", "3", "9", "https://images.vivino.com/thumbs/uX9zZ6CqS8KIWIGtqK7gGA_pb_x960.png");
+        dbHelper.saveToLocalDatabase("Savannah Rosé","Rosé","5","This sweet Rose has bold flavors of strawberry, peaches, and rose petal. Fruit-forward and sweet, this would be the perfect summer and spring drink to sip on all day long!", "1", "Rosé", "Texas", "1", "15", "https://filemanager-dfw.orderport.net/FiestaVineyardWinery/Catalog/40f862a1-c4b0-42b6-9bfb-d19c1b1a9f0d-Large.png?ts=637097745979370000");
+        dbHelper.saveToLocalDatabase("Hunt and Harvest Merlot","Merlot","1","The wine has lifted aromatics of red plum, dark cherry, and dusty milk chocolate. The fruit on the palate is bright with satsuma plum and red berries. The finish has touches of bitter dark chocolate.", "3", "White", "Napa Valley, California", "1", "25", "https://images.vivino.com/thumbs/uvX4ikh7QWqoRNHFbLJERA_pb_x600.png");
+        dbHelper.saveToLocalDatabase("F Bomb","Red Blend","2","Juicy aromas of strawberry jam, raspberry, and red plum jump out of the glass, with a light framing of toasty vanilla oak.", "3", "Red", "Santa Barbara County, California", "2", "20", "https://images.vivino.com/thumbs/3AR_6eNgSRyJhtu2tvrbAA_pb_x960.png");
+        dbHelper.saveToLocalDatabase("Lindeman's Bin 45 Cabernet","Cabernet Sauvignon","1","You can't go wrong with an affordable wine sporting deep color, good tannin structure, moderate acidity and aromas of blackcurrant, tomato leaf, dark spices and cedarwood.", "3", "Red", "Australia", "2", "7", "https://images.vivino.com/thumbs/YIaCtGWfSpOOQ-ZP9AmlOg_pb_x600.png");
     }
 
     public void openWineDishes()
