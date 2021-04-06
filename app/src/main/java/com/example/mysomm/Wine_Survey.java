513e43a3ae34aa774cc8219c28f9d7646702396b
Srinath <srinath.subramanian1@gmail.com>
2021-04-05 21:32:35 -0500

    Added Nick's wines to the database

--- app/src/main/java/com/example/mysomm/Wine_Survey.java
+++ app/src/main/java/com/example/mysomm/Wine_Survey.java
@@ -3,12 +3,51 @@ package com.example.mysomm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Wine_Survey extends AppCompatActivity {
+
+    private RadioGroup radioSweetGroup;
+    private RadioButton radioSweetButton;
+
+    private RadioGroup radioPriceGroup;
+    private RadioButton radioPriceButton;
+
+    private RadioGroup radioAcidGroup;
+    private RadioButton radioAcidButton;
+
+    private RadioGroup radioColorGroup;
+    private RadioButton radioColorButton;
+
+    private RadioGroup radioBodyGroup;
+    private RadioButton radioBodyButton;
+
+    private Button submitButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wine__survey);
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+    public void openSurveyWines()
+    {
+
+    }
+
+
 }
